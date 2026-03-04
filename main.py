03f4b8830770b68f77b7a8eeda61ca9c793cd50e
CS2 AA3 <cplugo@carc.pshs.edu.ph>
2026-03-04 13:54:45 +0800

    carisse did this!

--- main.py
+++ main.py
@@ -14,28 +14,44 @@ try:
 
     # Print an introduction
     print("Hello! Welcome to Stay In or Go Out : MBTI Tester")
-    print("Menu:")
-    answer = int(input(print("1. What is an MBTI? \n2. What does this program do?"
-                             "\n3. What is my MBTI?")))
+    print("\nMenu:")
+
+    print("1. What is an MBTI? \n2. What does this program do?"
+          "\n3. What is my MBTI? \n4. End")
+    answer = int(input("\nWhat is your choice? : "))
     print("-" * 100)
 
-    if answer == 1:
-        print("Myers-Briggs Type Indicator (MBTI) is a test where people get grouped into 16 personality types"
-              " based on their tendency or what they choose to do in certain situations. These personality types are"
-              " a combination of : Extroversion, Introversion, Sensing, Intuition, Feeling, Thinking, Prospecting, "
-              "Judging. This program was created to inform users about their MBTI and habits they usually do and their "
-              "personality after answering a set of questions.")
+    while answer != 4:
 
-    elif answer == 2:
-        print("This program not only gives you your MBTI, but it also provides you with what you usually do and your "
-              "personality! This program is very simple and you don't need to learn every single textbook definitions"
-              " as the questions would ask about what you experience.")
+        if answer == 1:
+            print("Myers-Briggs Type Indicator (MBTI) is a test where people get grouped into 16 personality types"
+                  " based on their tendency or what they choose to do in certain situations.\nThese personality types are"
+                  " a combination of : Extroversion, Introversion, Sensing, Intuition, Feeling, Thinking, Prospecting, "
+                  "Judging.\nThis program was created to inform users about their MBTI and habits they usually do and their "
+                  "personality after answering a set of questions.")
 
-    elif answer == 3:
-        for question in 
+            print("1. What is an MBTI? \n2. What does this program do?"
+                  "\n3. What is my MBTI? \n4. End")
+            answer = int(input("\nWhat is your choice? : "))
 
-    else:
-        print("This program has now been terminated, please re-open and input a valid choice.")
+        elif answer == 2:
+            print("This program not only gives you your MBTI, but it also provides you with what you usually do and your "
+                  "personality!\nThis program is very simple and you don't need to learn every single textbook definitions"
+                  " as the questions would ask about what you experience.")
+
+            print("1. What is an MBTI? \n2. What does this program do?"
+                  "\n3. What is my MBTI? \n4. End")
+            answer = int(input("\nWhat is your choice? : "))
+
+        elif answer == 3:
+            for question in data:
+
+
+        else:
+            print("Please  input a valid choice!")
+            print("1. What is an MBTI? \n2. What does this program do?"
+                  "\n3. What is my MBTI? \n4. End")
+            answer = int(input("\nWhat is your choice? : "))
 
 
 except FileNotFoundError:
